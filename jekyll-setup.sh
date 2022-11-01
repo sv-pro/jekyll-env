6e3b4978f50f1ddb8ad83c3754ead03d272c309c
Sergey Vlasov <sergeyvlasov.pro@gmail.com>
2022-11-01 20:31:38 +0000

    jekyll-setup.sh

--- jekyll-setup.sh
+++ jekyll-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+sudo apt-get install -y ruby-full build-essential zlib1g-dev
 
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
+
+gem install jekyll bundler
